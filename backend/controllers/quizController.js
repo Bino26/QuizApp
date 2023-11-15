0ec5c90650fecbceff5b49d497c126bd18a600ab
Bino26 <hbino1999@gmail.com>
2023-11-15 10:38:28 +0000

    Add Question Controller

--- backend/controllers/quizController.js
+++ backend/controllers/quizController.js
@@ -2,9 +2,37 @@ const questionModel = require("../models/questionSchema");
 const resultModel = require("../models/resultSchema");
 
 // questions controllers
-const getQuestions = async (req, res) => {};
-const insertQuestions = async (req, res) => {};
-const dropQuestions = async (req, res) => {};
+
+/** get all questions */
+const getQuestions = async (req, res) => {
+  try {
+    const questions = await questionModel.find();
+    res.status(200).json(questions);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+};
+/** insert all questinos */
+const insertQuestions = async (req, res) => {
+  try {
+    await questionModel.insertMany({ questions: [0], answers: [0] });
+    res.status(200).json({
+      message: "Data Saved Sucessfuly",
+    });
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+};
+
+/** Delete all Questions */
+const dropQuestions = async (req, res) => {
+  try {
+    await questionModel.deleteMany();
+    res.status(200).json({ message: "Questions Successfuly Deleted" });
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+};
 
 // result controllers
 
